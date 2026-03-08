6e9eabb23c4e2bc2acc67b5428e9582ec50b38e4
Aditya Gupta <adityaflautist@gmail.com>
2026-03-08 15:01:50 +0530

    Making all the services of auth

---
+++ 10MegaBlogProject/src/appwrite/auth.js
@@ -0,0 +1,56 @@
+import conf from "../conf/conf";
+import { Client, Account, ID } from "appwrite";
+
+export class AuthServices {
+    client = new Client();
+    /** @type {Account} */
+    account;
+    constructor() {
+        this.client
+            .setProject(conf.appwriteProjectId)
+            .setEndpoint(conf.appwriteUrl);
+        this.account = new Account(this.client)
+    }
+
+    async createAccount({ email, password, name }) {
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name);
+            if (userAccount) {
+                return this.login({ email, password });
+            } else {
+                return userAccount;
+            }
+        } catch (error) {
+            console.log("Appwrite service error create account: ", error)
+        }
+    }
+
+    async login({ email, password }) {
+        try {
+            return await this.account.createEmailPasswordSession(email, password);
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async getCurrentUser() {
+        try {
+            return await this.account.get();
+        } catch (error) {
+            console.log("Appwrite service error get current user: ", error)
+        }
+        return null;
+    }
+
+    async logout() {
+        try {
+            return await this.account.deleteSessions();
+        } catch (error) {
+            console.log("Appwrite service error logout: ", error)
+        }
+    }
+}
+
+const authServices = new AuthServices();
+
+export default authServices;
