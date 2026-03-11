71bc68ee1fc803337df09ea367c1990c90480e5f
Aditya Gupta <adityaflautist@gmail.com>
2026-03-11 23:34:35 +0530

    Made login and sign up page for website

--- 10MegaBlogProject/src/Components/index.js
+++ 10MegaBlogProject/src/Components/index.js
@@ -3,6 +3,12 @@ import Footer from './Footer/Footer'
 import Container from './container/Container'
 import Logo from './Logo'
 import LogoutBtn from './Header/LogoutBtn'
+import Login from './Login'
+import Signup from './Signup'
+import Button from './Button'
+import Input from './Input'
+import PostCard from './PostCard'
+import Select from './Select'
 
 export {
   Header,
@@ -10,5 +16,10 @@ export {
   Container,
   Logo,
   LogoutBtn,
-
+  Login,
+  Signup,
+  Button,
+  Input,
+  PostCard,
+  Select,
 }
